0df058642beeb3acfde6ee3f82bbf38eb1115b5f
Paul Jarvis <paul.jarvis@travisperkins.co.uk>
2016-02-22 13:57:18 +0000

    Unimplemented step defs for Checkout

---
+++ first-taste/checkout/step_definitions/CheckoutSteps.java
@@ -0,0 +1,24 @@
+package step_definitions;
+
+import cucumber.api.java.en.*;
+import cucumber.api.PendingException;
+
+public class CheckoutSteps {
+    @Given("^the price of a \"([^\"]*)\" is (\\d+)p$")
+    public void thePriceOfAIsP(String arg1, int arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I checkout (\\d+) \"([^\"]*)\"$")
+    public void iCheckout(int arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the total price should be (\\d+)p$")
+    public void theTotalPriceShouldBeP(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
